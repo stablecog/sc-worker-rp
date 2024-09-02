0811540e9e87fcc548a0170b69200d94fbbed5d1
yekta <ngyekta@gmail.com>
2024-09-02 03:36:28 +0300

    Relative import

--- src/shared/classes.py
+++ src/shared/classes.py
@@ -13,8 +13,8 @@ from diffusers import (
 from typing import Any, List, TypeVar
 from pydantic import BaseModel, Field, validator
 
-from src.shared.constants import SIZE_LIST
-from src.shared.schedulers import SD_SCHEDULER_DEFAULT
+from .constants import SIZE_LIST
+from .schedulers import SD_SCHEDULER_DEFAULT
 from PIL import Image
 
 T = TypeVar("T")
