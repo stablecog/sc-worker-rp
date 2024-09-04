7e9570280d356071c778779a4518262fd5bd8bfc
yekta <ngyekta@gmail.com>
2024-09-05 02:13:03 +0300

    Download aura sr in cuda builder

--- src/endpoints/aurasr/pipe.py
+++ src/endpoints/aurasr/pipe.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import torch
 from src.shared.pipe_classes import AuraSrPipeObject
 from aura_sr import AuraSR
 
@@ -14,8 +15,10 @@ def get_pipe_object(to_cuda: bool = True) -> AuraSrPipeObject:
 
 
 if __name__ == "__main__":
-    get_pipe_object(to_cuda=False)
-    """ logging.info("🔵 Skip downloading model")
-    folder = os.environ.get("HF_DATASETS_CACHE", None)
-    if folder is not None:
-        os.makedirs(folder, exist_ok=True) """
+    if torch.cuda.is_available():
+        get_pipe_object(to_cuda=False)
+    else:
+        logging.info("🔵 Skip downloading model")
+        folder = os.environ.get("HF_DATASETS_CACHE", None)
+        if folder is not None:
+            os.makedirs(folder, exist_ok=True)
