ef8432dc29b67ad093809ff863bf8be93c323ed8
yekta <ngyekta@gmail.com>
2024-09-04 21:33:43 +0300

    Remove device map

--- src/endpoints/flux1/pipe.py
+++ src/endpoints/flux1/pipe.py
@@ -2,20 +2,18 @@ import torch
 from typing import cast
 from diffusers import FluxPipeline
 from src.shared.pipe_classes import Flux1PipeObject
-from src.shared.device import DEVICE_CUDA, DEVICE_META
+from src.shared.device import DEVICE_CUDA
 
 MODEL_NAME = "FLUX.1"
 MODEL_ID = "black-forest-labs/FLUX.1-schnell"
 
 
 def get_pipe_object(to_cuda: bool = True) -> Flux1PipeObject:
-    device = DEVICE_META if to_cuda is False else None
     text2img = FluxPipeline.from_pretrained(
         MODEL_ID,
         torch_dtype=torch.bfloat16,
         add_watermark=False,
         safety_checker=None,
-        device_map=device,
     )
 
     if to_cuda:
