b292aeb76f13788393b69d9073237ee9c787f8cb
yekta <ngyekta@gmail.com>
2024-09-06 02:07:15 +0300

    Download flux model for now

--- src/endpoints/flux1/pipe.py
+++ src/endpoints/flux1/pipe.py
@@ -30,8 +30,8 @@ def get_pipe_object(to_cuda: bool = True) -> Flux1PipeObject:
 
 
 if __name__ == "__main__":
-    """ get_pipe_object(to_cuda=False) """
-    logging.info("🔵 Skip downloading model")
+    get_pipe_object(to_cuda=False)
+    """ logging.info("🔵 Skip downloading model")
     folder = os.environ.get("HF_DATASETS_CACHE", None)
     if folder is not None:
-        os.makedirs(folder, exist_ok=True)
+        os.makedirs(folder, exist_ok=True) """
